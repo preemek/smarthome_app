3ca291942ba8bc8c8f03faccee54e0f4fcedf600
no-logo <andrzej.wawryk@gmial.com>
2025-01-07 20:51:38 +0100

    Lekcja 2025-01-07

--- SmartHome/SmartHome/urls.py
+++ SmartHome/SmartHome/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from SmartHomeApp.views import shApp
+from django.conf.urls.static import static
+from django.conf import settings
 
 import SmartHomeApp
 
@@ -26,4 +28,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view()),
     path('', shApp, name='smart_home'),
     path('shApp/', include('SmartHomeApp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
