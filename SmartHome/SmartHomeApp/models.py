f9154525e29337352495eb8edfa5fb04bd8d4353
no-logo <andrzej.wawryk@gmial.com>
2025-01-09 15:38:52 +0100

    Praca w domu 2025-01-09

--- SmartHome/SmartHomeApp/models.py
+++ SmartHome/SmartHomeApp/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class DeviceManager(models.Manager):
+    def for_user(self, user):
+        return self.filter(owner=user)
+
+class Device(models.Model):
+    STATUS_CHOICES = (
+        (0, 'Off'), (1, 'On')
+    )
+    name = models.CharField(max_length=100, null=False, blank=False)
+    status = models.PositiveSmallIntegerField(null=False, blank=False, default=0, choices=STATUS_CHOICES)
+    power_in_watts = models.PositiveSmallIntegerField(null=False, blank=False, default=0)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True, default='no description')
+
+    objects = DeviceManager()
+
+    def __str__(self):
+        return self.name
--- SmartHome/SmartHomeApp/urls.py
+++ SmartHome/SmartHomeApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from SmartHomeApp.views import shApp_start
+
 urlpatterns = [
     path('start/', shApp_start, name='start'),
 ]
