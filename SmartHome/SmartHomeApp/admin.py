387e3df67506532350d0d1cd6cb451303bf7d12f
no-logo <andrzej.wawryk@gmial.com>
2025-01-09 17:28:25 +0100

    Praca w domu 2025-01-09

--- SmartHome/SmartHomeApp/admin.py
+++ SmartHome/SmartHomeApp/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from SmartHomeApp.models import Device
 
 # Register your models here.
-admin.site.register(Device)
+#admin.site.register(Device)
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ["name", "status", "owner"]
+    list_filter = ["owner"]
+    search_fields = ["name", "description"]
