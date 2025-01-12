e9652aac79d3c84d6dece30dfa8be4c244810767
no-logo <andrzej.wawryk@gmial.com>
2025-01-12 15:20:29 +0100

    Praca w domu 2025-01-12

--- SmartHome/SmartHomeApp/views.py
+++ SmartHome/SmartHomeApp/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
 from SmartHomeApp.models import Device
 from SmartHomeApp.forms import DeviceForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def shApp(request):
     return render(request, 'shApp.html')
 
+@login_required(login_url='login')
 def shApp_devices(request):
     devices = Device.objects.for_user(request.user)
     return render(request, 'shApp_devices.html', {"devices": devices})
 
+@login_required(login_url='login')
 def shApp_new_device(request):
     form = DeviceForm(request.POST or None)
 
@@ -23,6 +25,7 @@ def shApp_new_device(request):
     else:
         return render(request, 'shApp_device_form.html', {'form': form})
 
+@login_required(login_url='login')
 def shApp_edit_device(request, pk):
     device = get_object_or_404(Device, pk=pk)
     form = DeviceForm(request.POST or None, instance=device)
@@ -35,6 +38,7 @@ def shApp_edit_device(request, pk):
     else:
         return render(request, 'shApp_device_form.html', {'form': form})
 
+@login_required(login_url='login')
 def shApp_delete_device(request, pk):
     device = get_object_or_404(Device, pk=pk)
 
