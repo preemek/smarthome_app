7c8cb0776fb97a0e02cf6b3b94225323627edd50
no-logo <andrzej.wawryk@gmial.com>
2025-01-14 19:40:33 +0100

    Praca w domu 2025-01-14

--- SmartHome/SmartHomeApp/views.py
+++ SmartHome/SmartHomeApp/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='login')
 def shApp(request):
     return render(request, 'shApp.html')
 
