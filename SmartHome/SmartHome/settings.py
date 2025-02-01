44211745a23d7ef66f65430a3829ffcd4271a31b
no-logo <andrzej.wawryk@gmial.com>
2025-02-01 16:20:06 +0100

    remove secret and password from settings 2025-02-01

--- SmartHome/SmartHome/settings.py
+++ SmartHome/SmartHome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=2p+!(u4aiw%_7g7m#+htoj@&cf6jj3cccd3#95hp@p#9nsd^4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
@@ -86,11 +87,11 @@ WSGI_APPLICATION = 'SmartHome.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'defaultdb',
-        'USER': 'avnadmin',
-        'PASSWORD': 'AVNS_r6KmeNwbSlf59CUZewb',
-        'HOST': 'pg-smarthome-smarthome.k.aivencloud.com',
-        'PORT': '19416'
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
     }
 }
 
